79c3758e20fabeecadbee4b574d241f27a95a218
“plantoteachme” <amyplant@me.com>
2018-01-24 15:43:35 -0600

    Remove counter example

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = {
     options: ['one','two']
 };
 
-const template2 = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.title && <p>{app.subtitle}</p>}
@@ -19,37 +19,5 @@ const template2 = (
     </div>
 );
 
-let count = 0;
-
-const addOne = () => {
-    count ++;
-    renderCounterApp();
-};
-
-const minusOne = () => {
-    count --;
-    renderCounterApp();
-};
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
-
-
 const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-    const template = (
-        <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>reset</button>
-        </div>
-    );
-
-    ReactDOM.render(template, appRoot);
-}
-
-renderCounterApp();
+ReactDOM.render(template, appRoot);
