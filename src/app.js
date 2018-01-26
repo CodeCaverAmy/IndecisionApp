c81fcc1a60575f242c8349e77303fb25e6939ff4
“CodeCaverAmy” <amyplant@me.com>
2018-01-26 11:07:54 -0600

    Allow a child to communicate with its parent (by passing functions down to the children)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,46 @@
 
 class IndecisionApp extends React.Component {
+    // constructor
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.state = {
+            options: ['Thing One', 'Thing Two', 'Thing Three']
+        };
+    }
+    // methods (may need to be passed on to other components if they don't live in this render method)
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            };
+        });
+    }
+
+    handlePick() {
+        // randomly pick an option
+        const randomNumber = Math.floor(Math.random()*this.state.options.length);
+        const option = this.state.options[randomNumber];
+        alert(option);
+    }
+
+    //render
     render() {
-        const title = 'Indecision App';
-        const subtitle = 'Put your life in the hands of a computer';
-        let options = ['Thing One', 'Thing Two', 'Thing Three'];
         return (
             <div>
-                <Header title={title} subtitle={subtitle}/>
-                <Action />
-                <Options options={options}/>
-                <AddOption options={options}/>
+                <Header title={this.state.title} subtitle={this.state.subtitle}/>
+                <Action 
+                    hasOptions={this.state.options.length > 0}
+                    handlePick={this.handlePick}
+                />
+                <Options 
+                    options={this.state.options}
+                    handleDeleteOptions={this.handleDeleteOptions}
+                />
+                <AddOption 
+                    options={this.state.options}
+                />
             </div>
         );
     }
@@ -19,41 +50,37 @@ class IndecisionApp extends React.Component {
 class Header extends React.Component {
     // requires the render function
     render() {
+        const title = 'Indecision App';
+        const subtitle = 'Put your life in the hands of a computer';
         return (
             <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
+                <h1>{this.title}</h1>
+                <h2>{this.subtitle}</h2>
             </div>
         );
     }
 }
 
 class Action extends React.Component {
-    handlePick() {
-        alert('handlePick');
-    }
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                    onClick={this.props.handlePick}
+                    disabled={!this.props.hasOptions}
+                >
+                    What should I do?
+                </button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
-    constructor(props) {
-        // override and bind this.props to the handleRemoveAll method
-        super(props);
-        this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    }
-    handleRemoveAll() {
-        console.log(this.props.options);
-    }
     render() {
         return (
             <div>
-                <button onClick={this.handleRemoveAll}>Remove All</button>
+                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
                 <p>{this.props.options.length > 0 ? `Here are your ${this.props.options.length} options` : 'No options'}</p>
                 {
                     this.props.options.map((option) => <Option key= {option} optionText={option}/>)
