bc23d3eb9438a7c0c97d2e3668468a2672bfb946
“CodeCaverAmy” <amyplant@me.com>
2018-01-29 14:50:39 -0600

    Style the modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose = {props.handleClearSelectedOption} // allows user to click on the background or uses the escape key to close the modal
         contentLabel = "Selected Option" // users with accessibility settings enabled
+        closeTimeoutMS={200}
+        className='modal'
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick = {props.handleClearSelectedOption}>Great!</button>
+        <h3 className='modal__title'>Selected Option</h3>
+        {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+        <button className='button' onClick = {props.handleClearSelectedOption}>Great!</button>
     </Modal>
 );
 
