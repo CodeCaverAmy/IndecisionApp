7b50b2c579b8e7db8fa1ac8244f9d8f0d5ae6be6
“CodeCaverAmy” <amyplant@me.com>
2018-01-29 10:41:38 -0600

    Theming with Variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 // Header can be a stateless functional component, it only presents info, and doesn't manage state
 const Header = (props) => (
     <div className='header'>
-        <h1 className='header__title'>{props.title}</h1>
-        {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
+        <div className='container'>
+            <h1 className='header__title'>{props.title}</h1>
+            {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
+        </div>
     </div>
 );
 
